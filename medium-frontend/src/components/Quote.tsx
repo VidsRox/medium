89dc28e461128e6db73046d96b0e602abb6df3db
Vidyun Agarwal <vidyunagarwal@gmail.com>
2024-05-23 11:57:57 +0530

    Added frontend

---
+++ medium-frontend/src/components/Quote.tsx
@@ -0,0 +1,19 @@
+export const Quote = () => {
+    return <div className="bg-slate-200 h-screen flex justify-center flex-col">
+            <div className="flex justify-center">
+                <div className="max-w-lg ">
+                    <div className="text-3xl font-bold">
+                    "Customer service was exceptional and swiftly solved my query!"
+                    </div>
+                    <div className="max-w-md text-xl font-semibold mt-4">
+                    Julius Winfield
+                </div>
+                <div className="max-w-md text-sm font-light text-slate-400">
+                    CEO | Acme Corp.
+                </div>
+                </div>
+            </div>
+                
+                
+    </div>
+}
---
+++ medium-frontend/src/config.ts
@@ -0,0 +1,1 @@
+export const BACKEND_URL = "https://medium-backend.vidyun-medium-backend.workers.dev"
